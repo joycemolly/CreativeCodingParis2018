0ebe976e8b82014883e318749f815fe944454cb1
joycemolly <zhenq438@newschool.edu>
2018-09-28 09:37:36 +0200

    week4NoiseHomework

---
+++ Week4/Week4NoiseHomework/src/particles.cpp
@@ -0,0 +1,8 @@
+//
+//  particles.cpp
+//  Week4NoiseHomework
+//
+//  Created by 郑晴 on 9/28/18.
+//
+
+#include "particles.hpp"
---
+++ Week4/Week4NoiseHomework/src/particles.hpp
@@ -0,0 +1,13 @@
+//
+//  particles.hpp
+//  Week4NoiseHomework
+//
+//  Created by 郑晴 on 9/28/18.
+//
+
+#ifndef particles_hpp
+#define particles_hpp
+
+#include <stdio.h>
+
+#endif /* particles_hpp */
